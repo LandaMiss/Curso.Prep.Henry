bcbfddb0c4512d63e513577e4f1037ac5e6d82fe
LandaMiss <118214566+Gisela54@users.noreply.github.com>
2022-12-04 00:08:22 +0100

    Second Commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,18 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  var arr = [1, 2, 3,];
+  var max =
+  Math.max(...arr);
+  
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
-  //Si es menor, devolver --> "Not allowed"
+  //Si es menor, devolver --> "Not allowed"}
+  if(edad >=18 )return "Allowed"
+  if (edad < 18)return "Not allowed"
 }
   
 function conection(status) {
@@ -19,6 +25,9 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if(status = 1)return "Online"
+  if(status = 2)return "Away"
+  else return "Offline"
 }
 
 function saludo(idioma) {
@@ -28,6 +37,10 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if(idioma = aleman)return "Guten Tag!"
+  if(idioma = mandarin)return "Ni Hao!"
+  if(idioma = ingles)return "Hello!"
+  else return "Hola!"
 }
 
 function colors(color) {
@@ -38,18 +51,29 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  if(color = blue) return "This is blue"
+  if(color = red)return "This is red"
+  if(color =green)return "This is green"
+  if(color = orange)return "This is orange"
+  else return "Color not found"
+   
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero = 10, 5)return "true"
+   else return "false"
+
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero = 20 >= 50)return "true"
+   else return "false"
 }
 
 function esEntero(numero) {
@@ -60,6 +84,10 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  Number.isInterger = Number.isInteger
+  return typeof value === "number"
+  isInteger(value)
+  Math.floor(value) === value
 }
 
 function fizzBuzz(numero) {
--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,14 +6,26 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
-}
+    var object =  {   
+    nombre : nombre,
+    edad : edad,
+    meow : function () {
+    return "Meow!"
+    return object
+  }
+    }
+  }
 
 
-function agregarPropiedad(objeto, property) {
+   
+function agregarPropiedad (objeto, property) {
   // Agrega una propiedad al objeto (argumento "objeto") con el valor `null`
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto[property]= null
+  return objeto
+
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,12 +33,14 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto[metodo]()
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
+  return objetoMisterioso,NumeroDesconocido * 5
 
 }
 
@@ -35,28 +49,40 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+  delete objeto[unaPropiedad]
+  return objeto
 }
 
 function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
+  var obj= {
+    nombre: nombre,
+    email: email,
+    password: password,
+};
+return obj;
+ 
 
-}
 
 function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  if(usuario.email === null || usuario.email == undefined)return false;
+  else return true
+ 
 }
-
-
 
 function tienePropiedad(objeto, propiedad) {
   // Devuelve "true" si el objeto (parámetro "objeto") tiene una propiedad (key) cuyo nombre es igual al valor del argumento "propiedad"
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(objeto [propiedad]  === null || objeto[propiedad] == undefined) return false;
+  else return true
+
 }
 
 function verificarPassword(usuario, password) {
@@ -64,12 +90,16 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+  if (usuario.password === password)return true;
+  else return false;
 }
 
 function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password = nuevaPassword
+  return usuario
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
@@ -77,6 +107,8 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+  usuario.amigo.push(nuevoAmigo);
+  return usuario
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -85,16 +117,25 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  usuarios.forEach(element)}
+    element.espremium = true;
 }
+ return usuarios;
 
-function sumarLikesDeUsuario(usuario) {
+  function sumarLikesDeUsuario(usuario) {
   // "usuario" tiene una propiedad llamada "posts" que es un array
   // "posts" es un array de objetos "post"
   // Cada objeto "post" tiene una propiedad llamada "likes" que es un entero (int/integer)
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
-}
+
+  var suma = 0
+  for ( var i = 0; i < usuario . posts .length ;i ++ )  {
+    suma = suma + usuario. posts [i] .sumarLikesDeUsuario;
+  
+};
+  return suma.likesDeUsuario;
 
 function agregarMetodoCalculoDescuento(producto) {
   // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
@@ -106,6 +147,10 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+
+  producto.calcularPrecioDescuento = funcion() }
+    return this.precio - this.precio * this.porcentajeDeDescuento;
+    return producto
 
 }
 
